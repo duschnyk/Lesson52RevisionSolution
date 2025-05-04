6bb82782b06ee1bece9b8c302fa0a1a88c66ee91
giganig <matveykaschuk@gmail.com>
2025-05-04 18:33:42 +0300

    Task03 solved

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -9,6 +9,49 @@
 
 #include "logic.h"
 
-int count_extreme_elements(double* arr, int size) {
-	return -1;
+double get_max(double* array, int size) {
+	double max = array[0];
+
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+
+	return max;
+}
+
+double get_min(double* array, int size) {
+	double min = *array;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] < min) {
+			min = array[i];
+		}
+	}
+
+	return min;
+}
+
+int count_extreme_elements(double* array, int size) {
+	if (array == nullptr || size <= 0)
+	{
+		return 0;
+	}
+
+	double min = get_min(array, size);
+	double max = get_max(array, size);
+
+	int count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (*(array + i) == min || *(array + i) == max) {
+			count++;
+		}
+	}
+
+	return count;
 }
